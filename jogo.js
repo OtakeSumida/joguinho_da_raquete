61d1d54425ebce75f1cb16c5f669b646af52239e
OtakeSumida <98617470+OtakeSumida@users.noreply.github.com>
2022-01-28 17:18:16 -0300

    Create jogo.js

---
+++ jogo.js
@@ -0,0 +1,14 @@
+function setup() {
+  
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+ background("black")
+fill("pink")
+  rect(1,150,20,100);
+  rect(380,mouseY,20,100)
+  rect(190, 190, 20,20)
+  
+}
